bcd46b32b2daf1e740dbf7df3614b07e81dd2036
Cherced <68671184+Cherced@users.noreply.github.com>
2021-09-28 07:26:07 -0500

    Update Medium.js

--- TEST/Medium.js
+++ TEST/Medium.js
@@ -1,38 +1,54 @@
 let cabinets = ["100001011","1110000","11000000","11001000","100001110","10000101","1010010","1110010","11111100"];
-// for (let i = 0; i < cabinets.length; i++) {
-//     const element = cabinets[i];
-//     console.log(element);
-//     let lastNum = element.slice(-3);
-//     // console.log(lastNum)
-//     if (lastNum != 100) {
-//         lastNum = 100;
-//     }
-//     let numWithOut = element.slice(0, -3);
-//     // console.log(numWithOut);   
-//     // console.log(lastNum);
-//     let cabinetsCorrected = numWithOut+lastNum;
-//     console.log(cabinetsCorrected);
-// }
+let cabinets1 = ["100001011","1110000","11000000","11001000","100001110","10000101","1010010","1110010","11111100","100001011","1110000","11000000","11001000","100001110","10000101","1010010","1110010","11111100"];
+let cabinets2 = ["10000asd11","1110000","110asd00","11001000","1000asd10","10000101","1010010","1110010","11111100"];
+function itsBinaryCode(cabinets) {
+    let filter = /[01]+/;
+    let binaryChecker = new RegExp(filter);
+    let firstFilterCabinnet = []
+    for (let i = 0; i < cabinets.length; i++) {
+        let element = cabinets[i];
+        if (binaryChecker.test(element)) {
+           firstFilterCabinnet.push(element);
+        } else {
+           element = "";
+           firstFilterCabinnet.push(element);
+        }
+    }
+    return firstFilterCabinnet;
+}
+
+let quepaso = itsBinaryCode(cabinets2);
+console.log(quepaso);
+
 function cabinetChecker(cabinet) {
-    // if (condition) {
-        for (let i = 0; i < cabinet.length; i++) {
-            const element = cabinet[i];
-            let lastNum = element.slice(-3);
+    let cabinetsCorrected = [];
+    let numWithOut
+    let lastNum
+    let binaryCode = itsBinaryCode(cabinet);
+    if (binaryCode.length <= 9) {
+        for (let i = 0; i < binaryCode.length; i++) {
+            const element = binaryCode[i];
+            lastNum = element.slice(-3);
             if (lastNum != 100) {
                 lastNum = 100;
-            }
-            numWithOut = element.slice(0, -3);
-            let Corrected = numWithOut + lastNum;
-            let cabinetsCorrected = [];
-            cabinetsCorrected.push(Corrected); 
-        }
-                 
-    // }else {
-
-    // }
-    
+                numWithOut = element.slice(0, -3);
+                corrected = numWithOut + lastNum;
+                cabinetsCorrected.push(corrected); 
+            } else {
+                cabinetsCorrected.push("");    
+            }        
+        }     
+    return cabinetsCorrected; 
+    } else {
+        return ""
+    }
+           
 }
 
 let prueba1 = cabinetChecker(cabinets);
-console.log(cabinets);
-console.log("este es el nuevo cabbinet " + prueba1);
+let prueba2 = cabinetChecker(cabinets1);
+let prueba3 = cabinetChecker(cabinets2);
+console.log("este es el viejo cabbinet " + cabinets);
+console.log("este es el nuevo cabbinet1 " + prueba1);
+console.log("este es el nuevo cabbinet2 " + prueba2);
+console.log("este es el nuevo cabbinet3 " + prueba3);
